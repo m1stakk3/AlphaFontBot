952ec9f3e729a09dbed493c83ca03c0fcace9b7c
Daniel <dkirin60@gmail.com>
2021-11-02 18:54:40 +0300

    some

--- BOT Fonts/main.py
+++ BOT Fonts/main.py
@@ -5,7 +5,7 @@ bot = telebot.TeleBot("2018392915:AAFdEpg4G1RXvY8q3B6rBCSaFP-oXLA258s", parse_mo
 users = []
 
 
-
+# запуск бота + создание кнопок с примером текста
 @bot.message_handler(commands=['start'])
 def on_start(message: types.Message):
         name = message.from_user.username
@@ -18,6 +18,22 @@ def on_start(message: types.Message):
         buttons = [font1, 'Example', 'Example', 'Example']
         keyboard.add(*buttons)
         bot.send_message(message.chat.id, "Выберите желаемый шрифт:", reply_markup=keyboard)
+
+# обработчик текста
+@bot.message_handler(content_types=['text'])
+def changer(message: types.Message):
+    text_from_user = message.text.lower()
+    tlen = len(text_from_user)
+    text_donefor_user = ''
+    for i in range(tlen):
+        if ord(text_from_user[i]) in range(65, 123):
+            if text_from_user[i] == ' ':
+                text_donefor_user += ' '
+            else:
+                text_donefor_user += chr(127306 - (123 - ord(text_from_user[i])))
+        else:
+            pass
+    bot.send_message(message.chat.id, text_donefor_user)
 
 
 
